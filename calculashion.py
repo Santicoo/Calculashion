314d0a697742b22b88de023e823a95a6982555fb
Santicoo <santyy6406@gmail.com>
2024-09-09 13:31:59 -0500

    creación de la ventana
    llamamos la función ventana donde especificamos la medida de esta también su titulo y color

--- calculashion.py
+++ calculashion.py
@@ -1,3 +1,7 @@
     bg_color = "#F0F0F0"
     fg_color = "#333"
     font = ("Arial", 12)
+    vent = Tk()
+    vent.title("Calculadora Simple")
+    vent.geometry("600x400")
+    vent.configure(bg=bg_color)
