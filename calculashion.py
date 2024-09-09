578907da8b144d0e94dca0997213f3d85cd68ee2
Santicoo <santyy6406@gmail.com>
2024-09-09 13:44:23 -0500

    definición de la variable suma
    se crea la función de sumar donde tenemos 3 variables las cuales n1 y n2 las asignamos a los txt para que el usuario ingrese los números que quiera sumar y la variable r es donde se opera las otras dos

--- calculashion.py
+++ calculashion.py
@@ -6,3 +6,9 @@
     vent.geometry("600x400")
     vent.configure(bg=bg_color)
     global txt1, txt2, txt3
+def fnsuma():
+    n1 = txt1.get()
+    n2 = txt2.get()
+    r = float(n1) + float(n2)
+    txt3.delete(0, 'end')
+    txt3.insert(0, r)
