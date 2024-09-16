e7247fd938cc3d6176fe0f57acaaa2a29e44864f
Santicoo <santyy6406@gmail.com>
2024-09-15 21:08:42 -0500

    boton restar

--- calculashion.py
+++ calculashion.py
@@ -25,6 +25,8 @@ def crear_ventana():
     txt3.place(relx=0.5, rely=0.6, anchor='center', relwidth=0.6, relheight=0.1)
     btn_sumar = Button(vent, text="Sumar", command=fnsuma, font=font)
     btn_sumar.place(relx=0.2, rely=0.72, anchor='center', relwidth=0.2, relheight=0.1)
+    btn_restar = Button(vent, text="Restar", command=fnresta, font=font)
+    btn_restar.place(relx=0.4, rely=0.72, anchor='center', relwidth=0.2, relheight=0.1)
 def fnsuma():
     n1 = txt1.get()
     n2 = txt2.get()
